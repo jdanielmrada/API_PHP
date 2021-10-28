f765c3f829e573ac85e91fed67de3a10db187bcf
jdanielmrada <jdani9417@gmail.com>
2021-10-28 14:09:29 -0400

    practica v0.0.1

--- index.php
+++ index.php
@@ -6,16 +6,16 @@ require_once "controller/index.php";
 $controller = new Controller;
 
 #listar las citas
-// $tipoUser = 1;
-// print_r($controller->Listar($conexion , $tipoUser));
+$tipoUser = 1;
+print_r($controller->Listar($conexion , $tipoUser));
 
 #Pedir cita
-// $tipoUser = 0;
-// $detalle = 'descripcion';
+$tipoUser = 0;
+$detalle = 'descripcion';
     // ID del medico registrado
-// $user = 1;
+$user = 1;
 
-// print_r($controller->PedirCita($conexion , $tipoUser, $detalle,$user));
+print_r($controller->PedirCita($conexion , $tipoUser, $detalle,$user));
 
 #Actualizar cita
 $tipoUser = 1;
